ba02c5ffa4d7a39099d7f5e54523de0018b902b3
Shet0 <sheto.tk@gmail.com>
2024-10-01 20:01:19 -0500

    Relaciones vistas

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,6 +11,9 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_view.xml',
+        'views/estate_property_type_view.xml',
+        'views/estate_property_tag_view.xml',
+        'views/estate_property_offer_view.xml',
         'menus/menu.xml',
     ],
     # data files containing optionally loaded demonstration data
